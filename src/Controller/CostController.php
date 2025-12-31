f90a92d3c97c81659311e22d6dccf314baf62e33
antonio.pascucci <antonio.pascucci@performer.it>
2025-12-31 16:13:26 +0100

    Aggiunge il controller CRUD per i Cost e link in sidebar

---
+++ src/Controller/CostController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Cost;
+use App\Form\CostType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CostController extends BaseController
+{
+    public const CONTROLLER_NAME = 'CostController';
+
+    #[Route('/cost/index', name: 'app_cost_index', methods: ['GET'])]
+    public function index(EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+        $costs = $user ? $em->getRepository(Cost::class)->findAllForUser($user) : [];
+
+        return $this->render('cost/index.html.twig', [
+            'controller_name' => self::CONTROLLER_NAME,
+            'costs' => $costs,
+        ]);
+    }
+
+    #[Route('/cost/new', name: 'app_cost_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+        if (!$user instanceof \App\Entity\User) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $cost = new Cost();
+        $form = $this->createForm(CostType::class, $cost, ['user' => $user]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($cost);
+            $em->flush();
+
+            return $this->redirectToRoute('app_cost_index');
+        }
+
+        return $this->renderTurbo('cost/edit.html.twig', [
+            'controller_name' => self::CONTROLLER_NAME,
+            'cost' => $cost,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/cost/edit/{id}', name: 'app_cost_edit', methods: ['GET', 'POST'])]
+    public function edit(int $id, Request $request, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+        if (!$user instanceof \App\Entity\User) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $cost = $em->getRepository(Cost::class)->findOneForUser($id, $user);
+        if (!$cost) {
+            throw new NotFoundHttpException();
+        }
+
+        $form = $this->createForm(CostType::class, $cost, ['user' => $user]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            return $this->redirectToRoute('app_cost_index');
+        }
+
+        return $this->renderTurbo('cost/edit.html.twig', [
+            'controller_name' => self::CONTROLLER_NAME,
+            'cost' => $cost,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/cost/delete/{id}', name: 'app_cost_delete', methods: ['GET', 'POST'])]
+    public function delete(int $id, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+        if (!$user instanceof \App\Entity\User) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $cost = $em->getRepository(Cost::class)->findOneForUser($id, $user);
+        if (!$cost) {
+            throw new NotFoundHttpException();
+        }
+
+        $em->remove($cost);
+        $em->flush();
+
+        return $this->redirectToRoute('app_cost_index');
+    }
+}
---
+++ src/Form/CostType.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Cost;
+use App\Entity\CostCategory;
+use App\Entity\Ship;
+use App\Form\Type\TravellerMoneyType;
+use App\Repository\ShipRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $user = $options['user'];
+
+        $builder
+            ->add('title', TextType::class, [
+                'attr' => ['class' => 'input m-1 w-full'],
+            ])
+            ->add('amount', TravellerMoneyType::class, [
+                'label' => 'Amount (Cr)',
+                'attr' => ['class' => 'input m-1 w-full'],
+            ])
+            ->add('paymentDay', NumberType::class, [
+                'required' => false,
+                'attr' => ['class' => 'input m-1 w-full'],
+            ])
+            ->add('paymentYear', NumberType::class, [
+                'required' => false,
+                'attr' => ['min' => 1100, 'max' => 1300, 'class' => 'input m-1 w-full'],
+            ])
+            ->add('costCategory', EntityType::class, [
+                'class' => CostCategory::class,
+                'choice_label' => fn (CostCategory $cat) =>
+                    sprintf('%s - %s', $cat->getCode(), $cat->getDescription()),
+                'attr' => ['class' => 'select m-1 w-full'],
+            ])
+            ->add('ship', EntityType::class, [
+                'class' => Ship::class,
+                'placeholder' => '-- Select a Ship --',
+                'choice_label' => fn (Ship $ship) => sprintf('%s (%s)', $ship->getName(), $ship->getClass()),
+                'query_builder' => function (ShipRepository $repo) use ($user) {
+                    $qb = $repo->createQueryBuilder('s')->orderBy('s.name', 'ASC');
+                    if ($user) {
+                        $qb->andWhere('s.user = :user')->setParameter('user', $user);
+                    }
+                    return $qb;
+                },
+                'attr' => ['class' => 'select m-1 w-full'],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Cost::class,
+            'user' => null,
+        ]);
+    }
+}
