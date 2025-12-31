ddf7a4815e6f09a0e8e1022106497487c6da165c
antonio.pascucci <antonio.pascucci@performer.it>
2025-12-31 11:45:09 +0100

    Registra il listener Doctrine per assegnare l'utente

--- src/EventSubscriber/AssignUserSubscriber.php
+++ src/EventSubscriber/AssignUserSubscriber.php
@@ -7,44 +7,34 @@ use App\Entity\Mortgage;
 use App\Entity\MortgageInstallment;
 use App\Entity\Ship;
 use App\Entity\User;
-use Doctrine\Common\EventSubscriber;
-use Doctrine\ORM\Event\OnFlushEventArgs;
-use Doctrine\ORM\Events;
+use Doctrine\ORM\Event\PrePersistEventArgs;
+use Symfony\Bridge\Doctrine\Attribute\AsDoctrineListener;
 use Symfony\Bundle\SecurityBundle\Security;
 
-class AssignUserSubscriber implements EventSubscriber
+#[AsDoctrineListener(event: 'prePersist')]
+class AssignUserSubscriber
 {
     public function __construct(private readonly Security $security)
     {
     }
 
-    public function getSubscribedEvents(): array
-    {
-        return [Events::onFlush];
-    }
-
-    public function onFlush(OnFlushEventArgs $args): void
+    public function prePersist(PrePersistEventArgs $args): void
     {
         $user = $this->security->getUser();
         if (!$user instanceof User) {
             return;
         }
 
-        $em = $args->getObjectManager();
-        $uow = $em->getUnitOfWork();
+        $entity = $args->getObject();
 
-        foreach ($uow->getScheduledEntityInsertions() as $entity) {
-            if (
-                $entity instanceof Crew
-                || $entity instanceof Ship
-                || $entity instanceof Mortgage
-                || $entity instanceof MortgageInstallment
-            ) {
-                if (method_exists($entity, 'getUser') && method_exists($entity, 'setUser') && $entity->getUser() === null) {
-                    $entity->setUser($user);
-                    $meta = $em->getClassMetadata($entity::class);
-                    $uow->recomputeSingleEntityChangeSet($meta, $entity);
-                }
+        if (
+            $entity instanceof Crew
+            || $entity instanceof Ship
+            || $entity instanceof Mortgage
+            || $entity instanceof MortgageInstallment
+        ) {
+            if (method_exists($entity, 'getUser') && method_exists($entity, 'setUser') && $entity->getUser() === null) {
+                $entity->setUser($user);
             }
         }
     }
