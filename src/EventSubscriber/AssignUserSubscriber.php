53f06fc30d11dd0618d9d56cf8f09504265577b5
antonio.pascucci <antonio.pascucci@performer.it>
2025-12-31 10:17:26 +0100

    Assegna l'utente corrente alle entità salvate

---
+++ src/EventSubscriber/AssignUserSubscriber.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\Crew;
+use App\Entity\Mortgage;
+use App\Entity\MortgageInstallment;
+use App\Entity\Ship;
+use App\Entity\User;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Events;
+use Symfony\Component\Security\Core\Security;
+
+class AssignUserSubscriber implements EventSubscriber
+{
+    public function __construct(private readonly Security $security)
+    {
+    }
+
+    public function getSubscribedEvents(): array
+    {
+        return [Events::prePersist];
+    }
+
+    public function prePersist(LifecycleEventArgs $args): void
+    {
+        $user = $this->security->getUser();
+        if (!$user instanceof User) {
+            return;
+        }
+
+        $entity = $args->getObject();
+
+        if (
+            $entity instanceof Crew
+            || $entity instanceof Ship
+            || $entity instanceof Mortgage
+            || $entity instanceof MortgageInstallment
+        ) {
+            if (method_exists($entity, 'getUser') && method_exists($entity, 'setUser') && $entity->getUser() === null) {
+                $entity->setUser($user);
+            }
+        }
+    }
+}
