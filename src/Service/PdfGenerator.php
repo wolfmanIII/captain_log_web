94df264fd065b28304b5f25984649816f513d778
antonio.pascucci <antonio.pascucci@performer.it>
2026-01-01 14:20:14 +0100

    Aggiunge servizio PdfGenerator basato su Twig e Snappy

---
+++ src/Service/PdfGenerator.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Service;
+
+use Knp\Snappy\Pdf;
+use Twig\Environment;
+
+class PdfGenerator
+{
+    public function __construct(
+        private readonly Pdf $snappy,
+        private readonly Environment $twig,
+    ) {
+    }
+
+    /**
+     * Render a Twig template to HTML and convert it to PDF.
+     *
+     * @param string $template Twig template name (es. templates/contracts/FILE.html.twig)
+     * @param array  $context  Context passed to Twig
+     * @param array  $options  Snappy options (override config)
+     *
+     * @return string Binary PDF content
+     */
+    public function render(string $template, array $context = [], array $options = []): string
+    {
+        $html = $this->twig->render($template, $context);
+
+        return $this->snappy->getOutputFromHtml($html, $options);
+    }
+}
