e237ced2cac16af26256c717bbbf78537970d65a
Space Wolf <wolfmanIII@gmail.com>
2026-01-04 17:47:24 +0100

    Allinea template e form Campaign

--- src/Form/CampaignType.php
+++ src/Form/CampaignType.php
@@ -4,7 +4,7 @@ namespace App\Form;
 
 use App\Entity\Campaign;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -24,7 +24,7 @@ class CampaignType extends AbstractType
                 'required' => false,
                 'attr' => ['class' => 'textarea m-1 w-full', 'rows' => 3],
             ])
-            ->add('startingYear', IntegerType::class, [
+            ->add('startingYear', NumberType::class, [
                 'label' => 'Starting year',
                 'required' => false,
                 'attr' => ['class' => 'input m-1 w-full'],
