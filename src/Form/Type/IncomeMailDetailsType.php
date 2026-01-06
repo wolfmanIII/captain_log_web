01472200f903db890e2273653d736aa604d980ef
Space Wolf <wolfmanIII@gmail.com>
2026-01-06 02:11:00 +0100

    Corregge import IntegerType in dettagli posta

--- src/Form/Type/IncomeMailDetailsType.php
+++ src/Form/Type/IncomeMailDetailsType.php
@@ -8,6 +8,7 @@ use App\Form\Type\ImperialDateType;
 use App\Model\ImperialDate;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
